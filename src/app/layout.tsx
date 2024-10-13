c7b163d0ce345fc32215293cae830a9ea06132b5
Gedas Gardauskas <gedas.gardauskas@gmail.com>
2024-10-13 23:05:52 +0300

    fix: import error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ import { Snackbar } from 'src/components/snackbar';
 import { ProgressBar } from 'src/components/progress-bar';
 import { MotionLazy } from 'src/components/animate/motion-lazy';
 import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
+import { CSPostHogProvider } from './providers';
 
 // ----------------------------------------------------------------------
 
